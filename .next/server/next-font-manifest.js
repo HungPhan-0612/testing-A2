9d62aca4051d0449ec1ed11ebc5f6af0e4b70ee2
HungPhan-0612 <phanconghung0612@gmail.com>
2024-11-17 21:59:32 +0700

    First Commit

---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/Users/truongngochuyen/Downloads/Assignment2-COS30019-main copy/src/app/layout":["static/media/463dafcda517f24f-s.p.woff","static/media/4473ecc91f70f139-s.p.woff"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
